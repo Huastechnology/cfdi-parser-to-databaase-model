60f1651bfd248d19cba14a00b6afdd039ab8c915
vicenteyah <vicente.yah.dzul@icloud.com>
2022-05-30 15:21:23 -0500

    decorator was refactored

--- common/decorators/verify_tr_taxes.py
+++ common/decorators/verify_tr_taxes.py
@@ -1,50 +1,51 @@
+"""
+    @decorator that evaluates if the property <cfdi:Impuestos> has more properties
+"""
 def it_contains_properties(function):
     def wrapper(*args, **kwargs):
-        fn = function(*args, **kwargs)
+        keys = ['@TotalImpuestosRetenidos','@TotalImpuestosTrasladados']
 
-        if (
-            args[0].get("cfdi:Comprobante").get("cfdi:Impuestos").get("cfdi:Traslados")
-            is not None
-        ):
-            if (
-                args[0]
-                .get("cfdi:Comprobante")
-                .get("cfdi:Impuestos")
-                .get("@TotalImpuestosTrasladados")
-                is not None
-            ):
-                fn.update(
-                    {
-                        "total_taxes_tranfered": args[0]["cfdi:Comprobante"][
-                            "cfdi:Impuestos"
-                        ]["@TotalImpuestosTrasladados"]
-                    }
-                )
+        fn = function(*args, **kwargs)
+        if keys[1] and keys[0] in args[0]["cfdi:Comprobante"]["cfdi:Impuestos"]:
+            get_props = args[0].get("cfdi:Comprobante").get("cfdi:Impuestos")
+            if type(fn) is list:
+                fn.append({
+                    keys[1]: get_props[keys[1]],
+                    keys[0]: get_props[keys[0]]
+                })
+            elif(type(fn) is dict):
+                fn.update({
+                    keys[1]: get_props[keys[1]],
+                    keys[0]: get_props[keys[0]]
+                })
+            return fn
+        if keys[0] not in args[0]["cfdi:Comprobante"]["cfdi:Impuestos"]:
+            if keys[1] in args[0]["cfdi:Comprobante"]["cfdi:Impuestos"]:
+                get_props = args[0].get("cfdi:Comprobante").get("cfdi:Impuestos")
+                if type(fn) is list:
+                    fn.append({
+                        keys[1]: get_props[keys[1]]
+                    })
+                elif(type(fn) is dict):
+                    fn.update({
+                        keys[1]: get_props[keys[1]]
+                    })
                 return fn
-        if (
-            args[0]
-            .get("cfdi:Comprobante")
-            .get("cfdi:Impuestos")
-            .get("cfdi:Retenciones")
-            is not None
-        ):
-            fn.update(
-                {
-                    "total_taxes_retained": args[0]["cfdi:Comprobante"][
-                        "cfdi:Impuestos"
-                    ]["@TotalImpuestosRetenidos"]
-                }
-            )
             return fn
-        elif args[0].get("cfdi:Comprobante", {}).get("cfdi:Impuestos", {}) is not None:
-            fn.update(
-                {
-                    "total_taxes_tranfered": args[0]["cfdi:Comprobante"][
-                        "cfdi:Impuestos"
-                    ]["cfdi:Traslados"]["cfdi:Traslado"]["@Importe"]
-                }
-            )
+        if keys[1] not in args[0]["cfdi:Comprobante"]["cfdi:Impuestos"]:
+            if keys[0] in args[0]["cfdi:Comprobante"]["cfdi:Impuestos"]:
+                get_props = args[0].get("cfdi:Comprobante").get("cfdi:Impuestos")
+                if type(fn) is list:
+                    fn.append({
+                        keys[0]: get_props[keys[0]]
+                    })
+                elif(type(fn) is dict):
+                    fn.update({
+                        keys[0]: get_props[keys[0]]
+                    })
+                return fn
             return fn
+        
         return fn
 
     return wrapper
