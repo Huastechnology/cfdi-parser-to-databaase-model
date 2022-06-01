2803ec8dad7793bd146e2aa30ba75b22323cec86
vicenteyah <vicente.yah.dzul@icloud.com>
2022-06-01 16:19:13 -0500

    if contains currency property in each concept

--- common/decorators/verify_currency.py
+++ common/decorators/verify_currency.py
@@ -2,17 +2,29 @@ def it_contains_currency(func):
     def wrapper(*args, **kwargs):
         fn = func(*args, **kwargs)
         key = "@Moneda"
+        
         if key in args[0]["cfdi:Comprobante"]:
-            fn.update({"currency": args[0]["cfdi:Comprobante"]["@Moneda"]})
+            get_common = args[0].get("cfdi:Comprobante")
+            if(type(fn) is dict):
+                fn.update(
+                    {
+                        "currency": get_common[key]
+                    }
+                )
             return fn
-        elif key in args[0]["cfdi:Comprobante"]["cfdi:Conceptos"]["cfdi:Concepto"]:
-            fn.update(
-                {
-                    "currency": args[0]["cfdi:Comprobante"]["cfdi:Conceptos"][
-                        "cfdi:Concepto"
-                    ]["@Moneda"]
-                }
-            )
+        if key in args[0]["cfdi:Comprobante"]["cfdi:Conceptos"]["cfdi:Concepto"]:
+            get_props = args[0].get("cfdi:Comprobante").get("cfdi:Conceptos").get("cfdi:Concepto")
+            if type(fn) is list:
+                print(type(fn))
+                fn.append({
+                    "currency": get_props[key]
+                })
+            if(type(fn) is dict):
+                fn.update(
+                    {
+                        "currency": get_props[key]
+                    }
+                )
             return fn
         return fn
 
