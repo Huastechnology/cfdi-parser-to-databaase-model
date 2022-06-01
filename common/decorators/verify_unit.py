6da1bf4bca995700be2dbef0f8e713f7dce6f0d4
vicenteyah <vicente.yah.dzul@icloud.com>
2022-06-01 16:24:16 -0500

    if contains unit property in dict and list cases

--- common/decorators/verify_unit.py
+++ common/decorators/verify_unit.py
@@ -1,15 +1,22 @@
+import pprint
 def it_contains_unit(func):
     def wrapper(*args, **kwargs):
         fn = func(*args, **kwargs)
         key = "@Unidad"
         if key in args[0]["cfdi:Comprobante"]["cfdi:Conceptos"]["cfdi:Concepto"]:
-            fn.update(
-                {
-                    "unit": args[0]["cfdi:Comprobante"]["cfdi:Conceptos"][
-                        "cfdi:Concepto"
-                    ]["@Unidad"]
-                }
-            )
+            get_props = args[0].get("cfdi:Comprobante").get("cfdi:Conceptos").get("cfdi:Concepto")
+            if type(fn) is list:
+                fn.append(
+                    {
+                        "unit": get_props[key]
+                    }
+                )
+            if(type(fn) is dict):
+                fn.update(
+                    {
+                        "unit": get_props[key]
+                    }
+                )
             return fn
         return fn
 
